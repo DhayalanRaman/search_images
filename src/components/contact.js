c58a218a126fd66d20c119aded79442e1a751410
Dhayalan Raman <dhayalan_r@trigent.com>
2023-01-04 06:05:05 +0000

    test

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const contact = () => {
-  return(
-    <div className='ui raised very padded text container segment' style={{marignTop:'80px'}}>
+  return (
+    <div
+      className="ui raised very padded text container segment"
+      style={{ marignTop: "80px" }}
+    >
       <h1>Contact Us</h1>
-      <p>dhaya@123456@gmail.com</p>
+      <p>dhayalanrd2444@gmail.com</p>
       <p>Contact No:98***45***</p>
       <div>
-        <p>I am a fresher seeking for an opportunity to work in a challenging environment to prove 
-        my skills and utilize my knowledge and intelligence in the growth of the organization.
+        <p>
+          I am a fresher seeking for an opportunity to work in a challenging
+          environment to prove my skills and utilize my knowledge and
+          intelligence in the growth of the organization.
         </p>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
 export default contact;
